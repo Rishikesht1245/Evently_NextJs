6c6b4ea43cec387b92fbc7b1f392cabba12539f3
Rishikesh T <rishikeshtgit@gmail.com>
2024-02-26 13:00:11 +0530

    Update : created single event page - pending stripe integration

--- src/app/(root)/events/[id]/page.tsx
+++ src/app/(root)/events/[id]/page.tsx
@@ -1,12 +1,88 @@
-import EventForm from "@/components/shared/EventForm";
-import { auth } from "@clerk/nextjs";
+import { getEventById } from "@/lib/actions/event.actions";
+import { IEvent } from "@/lib/database/models/event.module";
+import { formatDateTime } from "@/lib/utils";
+import { SearchParamProps } from "@/types";
+import Image from "next/image";
 
-const SingleEvent = () => {
-  const { sessionClaims } = auth();
-  const userId = sessionClaims?.userId as string;
+const SingleEvent = async ({ params: { id } }: SearchParamProps) => {
+  const event: IEvent = await getEventById(id);
   return (
-    <section className="bg-primary-50 bg-dotted-pattern bg-center py-5 md:py-10">
-      Single Event Page
+    <section className="bg-primary-50 bg-dotted-pattern bg-center py-5 md:py-10 flex justify-center bg-contain sm:mx-10 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl mx-auto">
+        {/* We need to add the upload things domain in next config file */}
+        <Image
+          src={event?.imageUrl}
+          alt="Event image"
+          width={1000}
+          height={1000}
+          className="h-full min-h-[300px] object-cover"
+        />
+        {/* Details section */}
+        <div className="flex flex-col w-full gap-8 p-5 md:p-10">
+          {/* Checkout Button : Pending Stripe integration */}
+          <div className="flex flex-col gap-6">
+            <h2 className="h2-bold">{event.title}</h2>
+            <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
+              <p className="p-bold-20 px-5 rounded-full bg-green-500/10  py-2 text-green-400">
+                {event.isFree ? "Free" : `$${event?.price}`}
+              </p>
+              <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-600">
+                {event?.category?.name}
+              </p>
+              <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
+                by{" "}
+                <span className="text-primary-500">
+                  {event?.organizer?.firstName} {event?.organizer?.lastName}
+                </span>{" "}
+              </p>
+            </div>
+          </div>
+
+          {/* Date and location */}
+          <div className="flex flex-col gap-5 w-full">
+            {/* Date */}
+            <div className="flex gap-3 items-center">
+              <Image
+                src="/assets/icons/calendar.svg"
+                alt="calendar"
+                width={32}
+                height={32}
+              />
+              <div className="p-regular-16 lg:p-regular-16 flex flex-wrap items-center">
+                <p>
+                  {formatDateTime(event.startDateTime!).dateOnly} -{" "}
+                  {formatDateTime(event.startDateTime!).timeOnly} -{" "}
+                </p>
+
+                <p>
+                  {formatDateTime(event.endDateTime!).dateOnly} -{" "}
+                  {formatDateTime(event.endDateTime!).timeOnly}
+                </p>
+              </div>
+            </div>
+
+            {/* Location */}
+            <div className="flex items-center gap-3">
+              <Image
+                src="/assets/icons/location.svg"
+                alt="Location"
+                width={32}
+                height={32}
+              />
+              <p className="p-medium-16 md:p-regular-18">{event?.location}</p>
+            </div>
+          </div>
+
+          {/* Description and Url */}
+          <div className="flex flex-col gap-2">
+            <p className="p-bold-20 text-grey-600">What You'll Learn:</p>
+            <p className="p-medium-16 p-regular-18 ">{event?.description}</p>
+            <p className="p-medium-16 p-regular-18 truncate text-primary-500 underline">
+              {event?.url}
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- src/lib/actions/event.actions.ts
+++ src/lib/actions/event.actions.ts
@@ -5,6 +5,17 @@ import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import Event from "../database/models/event.module";
+import Category from "../database/models/category.model";
+
+export const populateEvent = async (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({ path: "category", model: Category, select: "_id name" });
+};
 
 export const CreateEvent = async ({
   event,
@@ -27,6 +38,22 @@ export const CreateEvent = async ({
     });
 
     return JSON.parse(JSON.stringify(newEvent));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getEventById = async (eventId: string) => {
+  try {
+    await connectToDatabase();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) {
+      throw new Error("Event not found");
+    }
+
+    return JSON.parse(JSON.stringify(event));
   } catch (error) {
     handleError(error);
   }
